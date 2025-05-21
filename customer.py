1a484f96e36bd984e4b2b7882f8b34f7b1077dee
Cynthia <cynthia.abuya@student.moringaschool.com>
2025-05-21 12:49:00 +0300

    Additional updates

--- customer.py
+++ customer.py
@@ -1,3 +1,5 @@
+from order import Order
+
 class Customer:
     def __init__(self, name):
         self.name = name
@@ -15,14 +17,12 @@ class Customer:
             raise ValueError("Name must be a string between 1 and 15 characters.")
 
     def orders(self):
-        return self._orders
+        return self._orders.copy()
 
     def coffees(self):
         return list({order.coffee for order in self._orders})
 
     def create_order(self, coffee, price):
-        from order import Order
-        order = Order(self, coffee, price)
-        self._orders.append(order)
-        coffee._orders.append(order)
-        return order
+        new_order = Order(self, coffee, price)
+        self._orders.append(new_order)
+        return new_order
--- order.py
+++ order.py
@@ -3,25 +3,44 @@ from coffee import Coffee
 
 class Order:
     def __init__(self, customer, coffee, price):
-        if not isinstance(customer, Customer):
-            raise TypeError("customer must be a Customer instance.")
-        if not isinstance(coffee, Coffee):
-            raise TypeError("coffee must be a Coffee instance.")
-        if not isinstance(price, (int, float)) or not (1.0 <= price <= 10.0):
-            raise ValueError("price must be a float between 1.0 and 10.0")
+        self.customer = customer
+        self.coffee = coffee
+        self.price = price  # Using property setter
+        
+        # Add this order to the customer's and coffee's lists
+        customer._orders.append(self)
+        coffee._orders.append(self)
 
-        self._customer = customer
-        self._coffee = coffee
-        self._price = price
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, value):
+        if not isinstance(value, float):
+            raise TypeError("Price must be a float")
+        if not 1.0 <= value <= 10.0:
+            raise ValueError("Price must be between 1.0 and 10.0")
+        if hasattr(self, '_price'):
+            raise AttributeError("Price cannot be changed after initialization")
+        self._price = value
 
     @property
     def customer(self):
         return self._customer
 
+    @customer.setter
+    def customer(self, value):
+        if not isinstance(value, Customer):
+            raise TypeError("Customer must be an instance of Customer class")
+        self._customer = value
+
     @property
     def coffee(self):
         return self._coffee
 
-    @property
-    def price(self):
-        return self._price
+    @coffee.setter
+    def coffee(self, value):
+        if not isinstance(value, Coffee):
+            raise TypeError("Coffee must be an instance of Coffee class")
+        self._coffee = value
